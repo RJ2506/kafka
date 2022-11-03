a25b1da063a6fe2c5ae89f029c9c26572fd04bc6
rj <rjgayem@gmail.com>
2022-11-02 19:50:49 -0700

    added a script for db

---
+++ db/drop_tables_mysql.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    user="rj", password="Rodolfjohn25!", host="localhost", database="mydb"
+)
+
+c = conn.cursor()
+c.execute(
+    """
+    DROP TABLE buying_products;
+    DROP TABLE search_products;
+    """
+)
+
+
+conn.close()
