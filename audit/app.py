dfaa0419b7dc6937b9a01158df6997bfe53ea16f
rj <rjgayem@gmail.com>
2022-11-02 22:51:12 -0700

    fix the error

--- audit/app.py
+++ audit/app.py
@@ -8,6 +8,7 @@ import json
 import yaml
 import logging, logging.config
 from pykafka import KafkaClient
+from pykafka.common import OffsetType
 
 with open("app_conf.yml", "r") as f:
     app_config = yaml.safe_load(f.read())
@@ -28,24 +29,56 @@ def get_purchase_item(index):
     # To prevent the for loop from blocking, we set the timeout to
     # 100ms. There is a risk that this loop never stops if the
     # index is large and messages are constantly being received!
+    consumer = topic.get_simple_consumer(consumer_group=b'event_group',reset_offset_on_start=True,consumer_timeout_ms=1000)
+
+    logger.info("Retrieving buy at index %d" % index)
+  
+    try:
+        for i,msg in enumerate(consumer):
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if i == index:
+                return msg['payload'], 201
+            else:
+                raise ValueError
+    except:
+        logger.error("No more messages found")
+
+   
+    
+    logger.error("Could not find buy at index %d" % index)
+
+    return { "message": "Not Found"}, 404
+def get_search_item(index):
+    """purchase the item you selected"""
+    server = f'{app_config["events"]["hostname"]}:{app_config["events"]["port"]}'
+    client = KafkaClient(hosts=server)
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+    # Here we reset the offset on start so that we retrieve
+    # messages at the beginning of the message queue.
+    # To prevent the for loop from blocking, we set the timeout to
+    # 100ms. There is a risk that this loop never stops if the
+    # index is large and messages are constantly being received!
     consumer = topic.get_simple_consumer(reset_offset_on_start=True,consumer_timeout_ms=1000)
 
     logger.info("Retrieving buy at index %d" % index)
 
     try:
-        for msg in consumer:
+        for i,msg in enumerate(consumer):
             msg_str = msg.value.decode('utf-8')
-            msg = json.load(msg_str)
-            print(msg)
-
+            msg = json.loads(msg_str)
+            if i == index:
+                return msg['payload'], 201
+            else:
+                raise ValueError
     except:
         logger.error("No more messages found")
 
-    logger.error("Could not find buy at index %d" % index)
+    logger.error("Could not find search at index %d" % index)
 
     return { "message": "Not Found"}, 404
 
 app = connexion.FlaskApp(__name__, specification_dir="")
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=9000)
