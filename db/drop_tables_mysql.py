31c929461bf5b3318202944f894b6cf3fd752b32
rj <rjgayem@gmail.com>
2022-11-02 19:58:51 -0700

    updated the db localhost

--- db/drop_tables_mysql.py
+++ db/drop_tables_mysql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 conn = mysql.connector.connect(
-    user="rj", password="Rodolfjohn25!", host="localhost", database="kafka"
+    user="rj", password="Rodolfjohn25!", host="acit3855-kafka-lab6a.eastus.cloudapp.azure.com", database="kafka"
 )
 
 c = conn.cursor()
